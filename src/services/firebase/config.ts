370ce51c4d36a1438d8d6066f43eef06480f0c21
TomRoum <tomi.roumio@gmail.com>
2026-03-27 21:52:37 +0200

    enabled async storage persistence for auth sessions

--- src/services/firebase/config.ts
+++ src/services/firebase/config.ts
@@ -1,6 +1,8 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth } from 'firebase/auth'
+// @ts-ignore
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 const firebaseConfig = {
     apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
@@ -13,5 +15,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 
-export const auth = getAuth(app)
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+})
 export const db = getFirestore(app)
